06a017b8cf517b557c6f17994a87c3809ac528c0
FranziskaHaisch <38277022+FranziskaHaisch@users.noreply.github.com>
2018-07-22 15:47:41 +0200

    Update Backfee_Startseite.php

--- HTML/Backfee_Startseite.php
+++ HTML/Backfee_Startseite.php
@@ -2,7 +2,44 @@
 
 include ("Backfee_Kopf.php");
 
-echo "<p> Dies ist eine Startseite </p>";
+class CStartseite {
+		public static function GenerateTagesRez($items, $class) {
+			$html = "<rez class='$class'>\n";
+
+			
+
+			$html .= '<p id="Rahmen1">RS</p>';
+			$html .= '<p id="Rahmen2"></p>';
+						
+			$html .= '<table  id="StartRahmenTR">
+            <tr>
+                <td rowspan="3" id="StartBildTR">
+                    <img src="Login_Bild.png" id="BildTR" title="Pizza Gafik" alt="Bild konnte nicht geladen werden" >
+                </td>
+                <td colspan="3"><h2><strong>Pizza</strong></h2></td>
+            </tr>
+            <tr>
+                <td colspan="3">Ein knuspriger Teig mit einer würzigen Tomatensoße und leckerem Belag</td>
+            </tr>
+            <tr>
+                <td>Mittel</td>
+                <td>1 Stunde</td>
+                <td>*****</td>
+            </tr>
+            </table>';
+
+            $html .= '<p id="Rahmen3"></p>';
+			$html .= '<p id="Rahmen4">LS</p>';
+
+            
+
+			$html .= "</rez>\n";
+			return $html;
+			
+		}
+	};
+
+	echo CStartseite::GenerateTagesRez($menu, 'tagesrez');
 
 include ("Backfee_Impressum.php")
 

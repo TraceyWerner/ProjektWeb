ffb4392f1b419a5e93bc2a37bc6cf98377ce8649
TraceyWerner <38276427+TraceyWerner@users.noreply.github.com>
2018-07-24 18:16:55 +0200

    Update DB_user.php

--- HTML/DB_user.php
+++ HTML/DB_user.php
@@ -37,55 +37,16 @@ function einloggen($name,$passwort) {
 
 
 // **************************
-// Passwort Wiederherstellung
-// **************************
-function antwortCheck($name,$antwort) {
-	global $mysqli;
-	// Vergessenes Passwort wiederherstellen vom User X mit der Antwort Z 
-	$query = "SELECT login.passwort FROM login WHERE login.antwort = '$antwort' AND login.name = '$name'";
-	printf("<b>Jetzt wird folgendes ausgeführt:</b> <i>%s</i><br>", $query);
-
-   if ($result = $mysqli->query($query)) {
-
- 		while($row = $result->fetch_array()) {
-  			printf("Das Passwort von User $name ist : %s %s <br/>",@$row["name"],$row["passwort"], @$row["frage"],@$row["antwort"]);
-  		}
-	}
-} // antwortCheck
-
-function passwortWiederherstellen($Name,$Antwort) {
-	global $mysqli;
-	// Frage des Users X einblenden bei Passwort Wiederherstellung
-	$name = $Name;
-	$antwort = $Antwort;
-	$query = "SELECT frage FROM login WHERE name = '$name'";
-	printf("<b>Jetzt wird folgendes ausgeführt:</b> <i>%s</i><br>", $query);
-
-	// SELECT anfgare schicken
-	if ($result = $mysqli->query($query)) {
-
-		while($row = $result->fetch_array()) {
-  			printf("Die Frage vom User $name ist : %s %s <br/>",@$row["name"],@$row["passwort"], $row["frage"],@$row["antwort"]);
- 		}
-	}
-	antwortCheck($name,$antwort);
-	
-}//passwortWiederherstellen
-
-
-// **************************
 // Passwort des Users ändern
 // **************************
 
 function passwortAendernLog($name, $passwortNeu, $passwortA, $passwortB) {
 	global $mysqli;
-	global $log;
 
 	$query = "UPDATE login Set passwort = '$passwortNeu' WHERE name = '".$mysqli->real_escape_string($name)."' AND passwort = '".$mysqli->real_escape_string($passwortA)."'";
 
 	if($passwortNeu == $passwortB) {
 		if ($result = $mysqli->query($query)) {
-			// Wenn eine Menge verfügbar, dann hat es geklappt.
 			return true;
 		}
 	}
@@ -99,15 +60,10 @@ function passwortAendern($name, $antwort, $passwortNeu, $passwort) {
 	$query = "UPDATE login SET passwort = '$passwortNeu' WHERE name = '".$mysqli->real_escape_string($name)."' AND antwort = '".$mysqli->real_escape_string($antwort)."'";
 
 	if($passwortNeu == $passwort) {
-
 		if($result = $mysqli->query($query)) {
-			echo "Ihr Passwort wurde erfolgreich geändert";
+
 		}
-		else
-			echo "Bitte prüfe noch einmal deine Eingaben";
 	}
-	else 
-		echo "Die passwörter müssen übereinstimmen!";
 
 } // passwortAendern
 
@@ -167,8 +123,6 @@ function meineRezepte($ich) {
 	}
 	else
 		return false;
-
-	
 }
 
 
@@ -179,7 +133,6 @@ function meineRezepte($ich) {
 function logout($name) {
 	global $mysqli;
 
-	$_SESSION["userName"] = "";
 	$_SESSION["name"] = "";
 }
 
@@ -188,8 +141,7 @@ function logout($name) {
 
 
 // MAIN
-
-if($_REQUEST['unameE'] && $_REQUEST['pswE'] ) {
+if(NULL != ($_REQUEST['unameE'] && $_REQUEST['pswE'])) {
 	if(einloggen($_REQUEST['unameE'],$_REQUEST['pswE'])) {
 		$_SESSION['name'] = $_REQUEST['unameE'];
 		include("startseite_pers.php");
@@ -197,21 +149,21 @@ if($_REQUEST['unameE'] && $_REQUEST['pswE'] ) {
 }
 else {
 	$frage = "Was ist deine Lieblingsfarbe?";
-	if($_REQUEST['unameR'] && $_REQUEST['sifrageR'] && $_REQUEST['pswNR']) {
+	if(NULL != ($_REQUEST['unameR'] && $_REQUEST['sifrageR'] && $_REQUEST['pswNR'])) {
 		registrieren($_REQUEST['unameR'], $_REQUEST['pswNR'],$frage, $_REQUEST['sifrageR']);
 		$_SESSION['name'] = $_REQUEST['unameR'];
 		include("startseite_pers.php");
 	}
 	else {
-		if($_SESSION["userName"] && $_REQUEST['pswN'] && $_REQUEST['pswA'] && $_REQUEST['pswB']) {
-			if(passwortAendernLog($_SESSION["userName"], $_REQUEST['pswN'], $_REQUEST['pswA'],$_REQUEST['pswB'])) {
+		if(NULL != ($_SESSION["name"] && $_REQUEST['pswN'] && $_REQUEST['pswA'] && $_REQUEST['pswB'])) {
+			if(passwortAendernLog($_SESSION['name'], $_REQUEST['pswN'], $_REQUEST['pswA'],$_REQUEST['pswB'])) {
 				include("PWaendern_erfolg.php");
 			}
 			else 
 				include("PWaendern_misserfolg.php");
 		}
 		else {
-			if($_REQUEST['unameV'] && $_REQUEST['sifrageV'] && $_REQUEST['pswNV'] && $_REQUEST['pswBV']) {
+			if(NULL != ($_REQUEST['unameV'] && $_REQUEST['sifrageV'] && $_REQUEST['pswNV'] && $_REQUEST['pswBV'])) {
 				passwortAendern($_REQUEST['unameV'], $_REQUEST['sifrageV'], $_REQUEST['pswNV'], $_REQUEST['pswBV']);
 				$_SESSION['name'] = $_REQUEST['unameV'];
 				include("startseite_pers.php");
@@ -222,11 +174,11 @@ else {
 					include("Favoriten.php");
 				}
 				if($knopf == "logout") {
-					logout($_SESSION["userName"]);
+					logout($_SESSION['name']);
 					include("startseite.php");
 				}
 				if($knopf == "meineRezepte") {
-					if(meineRezepte($_SESSION["name"])) {
+					if(meineRezepte($_SESSION['name'])) {
 						include("MeineRezepte.php");
 					}
 				}
@@ -234,12 +186,5 @@ else {
 		}
 	}
 }
-
-
-// Knopf value benutzen anstatt $Request !
-/*
-
-				else {
-				*/
 
 ?>

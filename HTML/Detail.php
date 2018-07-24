1e04f19a7c5dae9083ad8f45331c50b96674b96c
FranziskaHaisch <38277022+FranziskaHaisch@users.noreply.github.com>
2018-07-24 19:49:20 +0200

    Update Detail.php

--- HTML/Detail.php
+++ HTML/Detail.php
@@ -11,59 +11,53 @@
 
 		</style>
 	</head>
-
-	<header>
-		<h1>Backfee</h1> <!-- wird durch Logo ersetzt  -->
-		
-		<!-- Toolbar  -->
-		<table>
-			<colgroup>
-				<col width="180">
-				<col width="200">
-				<col width="200">
-			</colgroup>
-			<tr>
-				<th>Rezepte</th>
-				<th>Partyplaner</th>
-				<th>Willkommen, "USER123" </th> <!-- immer aktuellen User  -->
-			</tr>
-		</table>
-
-	</header>
 	
 	<body>
+		<header>
+			<?php include("Backfee_Kopf.php")?>
+		</header>
 
-		<h2>Cake Pops</h2> <!-- hier Namen des Rezepts ausgeben  -->
-		
-		
-		<img src="photo_2017-09-22_15-09-19.jpg" width="250" title="Test" alt="keinBildgefunden"><br>
+		<div class="detail">
+			<table>
+				<td>
+					<tr>
+						<h2>Cake Pops</h2> <!-- hier Namen des Rezepts ausgeben  -->
+					</tr>
+					<tr>
+						<img src="photo_2017-09-22_15-09-19.jpg" width="250" title="Test" alt="keinBildgefunden"><br>
+					</tr>			
+					<tr>
+						<h3> Zutaten: </h3>
+						<ul> 
+							<li> trockener Kuchen </li>
+							<li>Frischkäse</li>
+							<li>Schokolade</li>
+							<li>Dekostreusel</li>
+							<li>CakePop-Stiele oder Schaschlikspieße</li>
+						</ul>
+					</tr>
+					<tr>
+						<button type="submit">zu Favoriten hinzufügen</button>
+					</tr>
+					<tr>
+						<h3> Zubereitung: </h3>
+						<ol> 
+							<li> Trockenen Kuchen zerkrümmeln und mit dem Frischkäse vermengen. </li>
+							<li> Aus dem Teig kleine Kugeln formen und kurz in den Kühlschrank stellen. </li>
+							<li> Schokolade über dem Wasserbad schmelzen.</li>
+							<li> CakePop-Stiele in die Schokolade tauchen und dann in die Kuchenkugel stecken und trocknen lassen. </li>
+							<li> Kuchenkugel in Schokolade tauchen und abtropfen lassen und mit Streuseln dekorieren. </li>
+						</ol>
+					</tr>
+					<tr>
+						<p> erstellt von <a href="">"User123"</a> </p>
+					</tr>	
+				</td>		
+			</table>
+		</div>
 
-		<h3> Zutaten: </h3>
-		<ul> 
-			<li> trockener Kuchen </li>
-			<li>Frischkäse</li>
-			<li>Schokolade</li>
-			<li>Dekostreusel</li>
-			<li>CakePop-Stiele oder Schaschlikspieße</li>
-		</ul>
-		
-		<button type="submit">zu Favoriten hinzufügen</button>
-
-		<h3> Zubereitung: </h3>
-		<ol> 
-			<li> Trockenen Kuchen zerkrümmeln und mit dem Frischkäse vermengen. </li>
-			<li> Aus dem Teig kleine Kugeln formen und kurz in den Kühlschrank stellen. </li>
-			<li> Schokolade über dem Wasserbad schmelzen.</li>
-			<li> CakePop-Stiele in die Schokolade tauchen und dann in die Kuchenkugel stecken und trocknen lassen. </li>
-			<li> Kuchenkugel in Schokolade tauchen und abtropfen lassen und mit Streuseln dekorieren. </li>
-		</ol>		
-
-		<p> hinzufügen - Partyplaner <select>
-  				<option value="test">test</option>
-  				<option value="bla">bla</option>
- 				</select>
-			</p>
-
-		<p> erstellt von <a href="">"User123"</a> </p>
+		<footer>
+			<?php include("Backfee_Impressum.php")?>
+		</footer>
 	</body>
 </html>
